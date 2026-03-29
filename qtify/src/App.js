980711cd41766df00dae104943d008cc9ad33be2
shiv bahadur <shivyadav7355@gmail.com>
2026-03-29 15:25:29 +0530

    navbar

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import Hero from './components/Hero/Hero';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div>
-     
+     <Navbar/>
       <Hero/>
     </div>
   );
